7b57a8508b8a6c0b4066ee26c9da32274f011b3f
RyanF14 <ryan.fellin25@ycdsbk12.ca>
2024-09-13 11:03:54 -0400

    Review 1 and 2 completed

--- src/gr11review/part1/Review2.java
+++ src/gr11review/part1/Review2.java
@@ -1,1 +1,34 @@
 package gr11review.part1;
+import java.io.*;
+
+public class Review2{
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int joke;
+        BufferedReader Keyboard = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("0 - print a joke about hair");
+        System.out.println("1 - print a joke about feet");
+        System.out.println("2 - Print a joke about clothes");
+        System.out.println("3 - print a joke about teachers");
+        System.out.print("Select: ");
+        joke = Integer.parseInt(Keyboard.readLine());
+        switch (joke) {
+            case 0:
+                System.out.println("you want to know the most unfortunate name?");
+                System.out.println("My mother had a friend named harrison butt, he lived a hard life in high school. Nice guy though apparently");
+                break;
+            case 1:
+                System.out.println("🤨");
+                break;
+            case 2:
+                System.out.println("this joke will knock your socks off");
+                break;
+            case 3:
+                System.out.println("what's the deal with teachers, am i right?");
+                break;
+
+        
+            
+        }
+
+    }
+}
